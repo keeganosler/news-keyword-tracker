1a40b2508c462ad59a4df574a3bccb26715a5983
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-04 09:24:36 -0600

    Update news-keyword-tracker.py

--- news-keyword-tracker.py
+++ news-keyword-tracker.py
@@ -5,6 +5,13 @@ import bs4, requests, smtplib
 #email address to send info to
 email_address = 'example@email.com'
 
-#keywords to seac\rch
+#keywords to search
 keywords = ['keyword1', 'keyword2', 'keyword3']
 any_present = False
+
+#news sites to search
+websites = ['https://www.cbc.ca/news', 'https://www.cbc.ca/news/canada', 'https://www.cbc.ca/news/canada/north']
+
+#download and parse page
+
+
