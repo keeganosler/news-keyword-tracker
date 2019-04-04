23cac17c831535e6237688faf21b8589dddd06ce
Keegan Osler <45800880+keeganosler@users.noreply.github.com>
2019-04-04 09:20:35 -0600

    Create news-keyword-tracker.py

---
+++ news-keyword-tracker.py
@@ -0,0 +1,10 @@
+#! python3
+
+import bs4, requests, smtplib
+
+#email address to send info to
+email_address = 'example@email.com'
+
+#keywords to seac\rch
+keywords = ['keyword1', 'keyword2', 'keyword3']
+any_present = False
